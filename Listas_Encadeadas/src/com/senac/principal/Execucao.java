53408a8f8117eb9b53442e29c6f522e4f1d4d424
msndogui@hotmail.com.br <msndogui@hotmail.com.br>
2014-03-12 23:58:12 -0300

    Adicionando o Menu e Criando Metodos.

--- Listas_Encadeadas/src/com/senac/principal/Execucao.java
+++ Listas_Encadeadas/src/com/senac/principal/Execucao.java
@@ -7,33 +7,69 @@ public class Execucao {
 
 	public static Arquivo registro = new Arquivo();
 	public static String opcao = "nao";
-
-	public static void main(String[] args) {
+	public static int funcao=0;
+	public static Scanner sc=new Scanner(System.in);
+	
+	public static ListaOrdenada<String> lista = new ListaOrdenada<String>();
+	public static Nodo<String> nodo = new Nodo();
+	
+	
+	public static void insereContatos(){
 		String nome;
 		String telefone;
 
-		Scanner sc=new Scanner(System.in);
-
-		System.out.println("Lista Telefonica:\n");
-		System.out.println("O que você deseja fazer?" +
-							"1 - Inserir" +
-							"2 - Remover" +
-							"3 - Pesquisar");
+		System.out.println("Digite o nome a ser inserido:");
+		nome = sc.next();
+		System.out.println("Digite o numero a ser inserido:");
+		telefone = sc.next();
+		lista.insert(new Nodo<String>(nome));
+		lista.insert(new Nodo<String>(telefone));
+		registro.criaArquivo(nome, telefone);
+	}
+	
+	public static void removeContatos(){
 		
-		while(opcao.equals("nao")){
-			System.out.println("Você Deseja Inserir novos contatos?");
-			
-			System.out.println("Digite o nome a ser inserido:");
-			nome = sc.next();
-			System.out.println("Digite o numero a ser inserido:");
-			telefone = sc.next();
-			registro.criaArquivo(nome, telefone);
+	}
+	
+	public static void pesquisaContatos(){
+		
+	}
 
+	public static void mostraContatos(){
+		lista.print();
+		
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("Lista Telefonica:\n");
+
+		while(opcao.equals("nao")){
+			System.out.println("O que você deseja fazer?" +
+					"\n1 - Inserir" +
+					"\n2 - Remover" +
+					"\n3 - Pesquisar");
+			funcao = sc.nextInt();
+			switch(funcao){
+			case 1 :
+				insereContatos();
+				break;
+			case 2 :
+				removeContatos();
+				break;
+			case 3 :
+				pesquisaContatos();
+				break;
+			case 4 :
+				mostraContatos();
+				break;
+			default :
+				System.out.println("Opcao Invalida!");
+			}
 			System.out.println("Você deseja sair?");
 			opcao = sc.next();
-			
+
 		}
-		
+
 	}
 
 }
